e0b86f4e44b2fa30d943f0a93b9788b96cae518c
Deon Jaz Grinham <61841531+Timmytwosmokes@users.noreply.github.com>
2024-06-14 11:52:59 +0100

    Update and rename BPG_CLPrintCommercialInvoiceButton.js to CLPrintCommercialInvoiceButton.js

--- CLPrintCommercialInvoiceButton.js
+++ CLPrintCommercialInvoiceButton.js
@@ -9,13 +9,13 @@ define(["N/currentRecord", "N/url"], function(currentRecord, url) {
         // Do Nothing
     }
 
-    function BPG_CLPrintCommercialInvoiceButton() {
+    function CLPrintCommercialInvoiceButton() {
         var recordId = currentRecord.get().id;
 
         
         var slUrl = url.resolveScript({
-            scriptId: 'customscript_bpg_custom_sl_print_comminv',
-            deploymentId: 'customdeploy_bpg_custom_sl_print_comminv',
+            scriptId: 'customscript_custom_sl_print_comminv',
+            deploymentId: 'customdeploy_custom_sl_print_comminv',
             params: { recordId: recordId } // Pass any necessary parameters to the Suitelet
         });
         window.open(slUrl);
@@ -23,7 +23,7 @@ define(["N/currentRecord", "N/url"], function(currentRecord, url) {
 
     return {
         pageInit: pageInit,
-        BPG_CLPrintCommercialInvoiceButton: BPG_CLPrintCommercialInvoiceButton
+        CLPrintCommercialInvoiceButton: CLPrintCommercialInvoiceButton
     };
 
 });
